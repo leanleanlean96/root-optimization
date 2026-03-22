26f5bc9df02d14b5742e6a6bbcc3ccdb807a8295
leanleanlean96 <kiraaff2289@gmail.com>
2026-03-22 11:53:44 +0300

    add sqla naming conventions

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,14 @@ class DbConfig(BaseModel):
     pool_size: int = 25
     max_overflow: int = 10
 
+    naming_convention = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
+
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
--- app/models/base.py
+++ app/models/base.py
@@ -1,7 +1,12 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 
+from core.config import config
 
 class Base(DeclarativeBase):
+    metadata = MetaData(
+        naming_convention=config.db.naming_convention,
+    )
     pass
 
 
