f32f2ab9a514ce95b253cd4b8ee10cd895a28bde
leanleanlean96 <kiraaff2289@gmail.com>
2026-04-16 23:11:13 +0300

    Feat/route generator (#10)
    * refactor: replace tuple[float, float] with Coordinate domain model
    
    * feat(config): add GeoConfig with bounding box settings
    
    * fix imports

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -3,27 +3,42 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import AsyncGenerator
 
+from app.application.use_cases.create_route import CreateRouteUseCase
+from app.application.use_cases.delete_route import DeleteRouteUseCase
+from app.application.use_cases.generate_random_coordinates import (
+    GenerateRandomCoordinatesUseCase,
+)
+from app.application.use_cases.get_route import GetRouteByIdUseCase
+from app.application.use_cases.get_route_metrics import GetRouteMetricsUseCase
+from app.application.use_cases.optimize_route import OptimizeRouteUseCase
 from app.core.auth.auth_service import JwtAuthService
 from app.core.auth.models import UserClaims
 from app.core.config import config
+from app.core.exceptions import (
+    InvalidTokenException,
+    InvalidTokenTypeException,
+    TokenExpiredException,
+    UnauthorizedException,
+)
 from app.data.dbclient import db_client
-from app.core.exceptions import UnauthorizedException
-from app.core.exceptions import TokenExpiredException, InvalidTokenException, InvalidTokenTypeException
 from app.data.repositories.routes_repository import RouteRepository
+from app.domain.models.coordinate import BoundingBox
+from app.domain.services.coordinate_generator_service import CoordinateGenerator
+from app.infrastructure.coordinate_generator import (
+    CoordinateGenerator,
+)
 from app.infrastructure.osrm_client import OsrmClient
-from app.application.use_cases.create_route import CreateRouteUseCase
-from app.application.use_cases.optimize_route import OptimizeRouteUseCase
-from app.application.use_cases.get_route import GetRouteByIdUseCase
-from app.application.use_cases.get_route_metrics import GetRouteMetricsUseCase
-from app.application.use_cases.delete_route import DeleteRouteUseCase
+
 
 async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
     async for session in db_client.session_getter():
         yield session
 
+
 async def get_http_client() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(timeout=30.0) as client:
         yield client
+
 
 def get_auth_service() -> JwtAuthService:
     return JwtAuthService(
@@ -33,19 +48,25 @@ def get_auth_service() -> JwtAuthService:
         algorithm=config.jwt.algorithm,
     )
 
+
 async def get_user_claims(
     auth_string: str = Header(...),
-    auth_service: JwtAuthService = Depends(get_auth_service)
+    auth_service: JwtAuthService = Depends(get_auth_service),
 ) -> UserClaims:
     try:
         token = auth_string.removeprefix("Bearer ")
         return auth_service.get_payload_data(token)
-    #TODO: add custom JWTservice exceptions
-    except (TokenExpiredException, InvalidTokenException, InvalidTokenTypeException) as e:
+    except (
+        TokenExpiredException,
+        InvalidTokenException,
+        InvalidTokenTypeException,
+    ) as e:
         raise UnauthorizedException(f"Unauthorized: {e}")
 
+
 def get_osrm_client(
-    service_url: str = config.osrm.url, client: AsyncClient = Depends(get_http_client)
+    service_url: str = config.osrm.url,
+    client: AsyncClient = Depends(get_http_client),
 ) -> OsrmClient:
     return OsrmClient(service_url, client)
 
@@ -54,6 +75,19 @@ def get_route_repo(
     session: AsyncSession = Depends(get_db_session),
 ) -> RouteRepository:
     return RouteRepository(session)
+
+
+def get_bbox() -> BoundingBox:
+    return BoundingBox(
+        min_lat=config.geo.min_lat,
+        min_lon=config.geo.min_lon,
+        max_lat=config.geo.max_lat,
+        max_lon=config.geo.max_lon,
+    )
+
+
+def get_coordinate_generator() -> CoordinateGenerator:
+    return CoordinateGenerator()
 
 
 def get_create_route_usecase(
@@ -80,7 +114,15 @@ def get_delete_route_usecase(
 ) -> DeleteRouteUseCase:
     return DeleteRouteUseCase(route_repo=route_repo)
 
+
 def get_optimize_route_usecase(
-    osrm_client: OsrmClient = Depends(get_osrm_client)
+    osrm_client: OsrmClient = Depends(get_osrm_client),
 ) -> OptimizeRouteUseCase:
     return OptimizeRouteUseCase(osrm_client=osrm_client)
+
+
+def get_generate_random_coordinates_usecase(
+    generator: CoordinateGenerator = Depends(get_coordinate_generator),
+    bbox: BoundingBox = Depends(get_bbox),
+) -> GenerateRandomCoordinatesUseCase:
+    return GenerateRandomCoordinatesUseCase(generator=generator, bbox=bbox)
