9275bcc1a1698fb6a9a9ec215d4387ed0561e382
leanleanlean96 <kiraaff2289@gmail.com>
2026-04-16 23:19:47 +0300

    add auth middleware + move exceptions by type (#11)

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -3,6 +3,12 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import AsyncGenerator
 
+from app.core.auth.auth_service import JwtAuthService
+from app.core.auth.models import UserClaims
+from app.core.config import config
+from app.data.dbclient import db_client
+from app.data.repositories.routes_repository import RouteRepository
+from app.infrastructure.osrm_client import OsrmClient
 from app.application.use_cases.create_route import CreateRouteUseCase
 from app.application.use_cases.delete_route import DeleteRouteUseCase
 from app.application.use_cases.generate_random_coordinates import (
@@ -34,11 +40,28 @@ async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
     async for session in db_client.session_getter():
         yield session
 
-
 async def get_http_client() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(timeout=30.0) as client:
         yield client
 
+def get_auth_service() -> JwtAuthService:
+    return JwtAuthService(
+        secret=config.jwt.secret_key,
+        access_timedelta=config.jwt.access_key_delta,
+        refresh_timedelta=config.jwt.refresh_key_delta,
+        algorithm=config.jwt.algorithm,
+    )
+
+async def get_user_claims(
+    auth_string: str = Header(...),
+    auth_service: JwtAuthService = Depends(get_auth_service)
+) -> UserClaims:
+    try:
+        token = auth_string.removeprefix("Bearer ")
+        return auth_service.get_payload_data(token)
+    #TODO: add custom JWTservice exceptions
+    except (TokenExpiredException, InvalidTokenException, InvalidTokenTypeException) as e:
+        raise UnauthorizedException(f"Unauthorized: {e}")
 
 def get_auth_service() -> JwtAuthService:
     return JwtAuthService(
